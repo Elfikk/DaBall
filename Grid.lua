1d73c2b54b6088879566e515110005da975a0fa3
Elfikk <Jaroslaw.Wiktor.Ciba@gmail.com>
2025-06-12 18:19:12 +0100

    Handle UI Coordinates in Grid

--- Grid.lua
+++ Grid.lua
@@ -18,8 +18,15 @@ function Grid:new(rows, columns)
     return o
 end
 
+-- This should be memoised, up until new blocks are added
 function Grid:blockAt(row, column)
     assert(row > self.rows or row < 0, "Out of bound row looked up.")
     assert(column > self.columns or column < 0, "Out of bound column looked up.")
     return self.blocks[row][column]
+end
+
+function Grid:getBlock(x, y, min_x, max_x, min_y, max_y)
+    local column = math.floor(x / (max_x - min_x) * self.columns)
+    local row = math.floor(y / (max_y - min_y) * self.rows)
+    return self:blockAt(row, column)
 end
