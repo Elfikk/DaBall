c34b459480565d66e3b2bc9013fa1c73d4240584
Elfikk <Jaroslaw.Wiktor.Ciba@gmail.com>
2025-06-15 01:17:32 +0100

    Add Circle for Powerups

---
+++ Circle.lua
@@ -0,0 +1,49 @@
+require("PositionVector")
+
+Circle = {
+    radius = 1,
+    position = PositionVector:new(0, 0),
+    hitpoints = 1 -- This never changes
+}
+
+function Circle:new(x, y, r)
+    -- Create a new ball object
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    self.radius = r
+    self.position = PositionVector:new(x, y)
+    return o
+end
+
+function Circle:inside(otherPos)
+    local diffPos = self.position - otherPos
+    local diffSq = diffPos:dot(diffPos)
+    local isInside = diffSq < self.radius * self.radius
+
+    print(self.position.x, self.position.y)
+    print(otherPos.x, otherPos.y)
+    print(diffSq)
+    print("")
+
+    if isInside then
+        self.hitpoints = self.hitpoints - 1
+    end
+    return isInside
+end
+
+function Circle:getPos()
+    return self.position
+end
+
+function Circle:getRadius()
+    return self.radius
+end
+
+function Circle:moveDown(delta_y)
+    self.position = self.position + PositionVector:new(0, delta_y)
+end
+
+function Circle:isValid()
+    return self.hitpoints ~= 0
+end
