33a47f5f52fdb4daad6c9c9e0db5ebd1c9771f1a
Elfikk <Jaroslaw.Wiktor.Ciba@gmail.com>
2025-07-03 09:55:42 +0100

    Add Outside Text Alignment

--- ShapeDrawer.lua
+++ ShapeDrawer.lua
@@ -6,6 +6,7 @@ TextAlignment = {
     RIGHT = 0x4,
     TOP = 0x8,
     BOTTOM = 0x10,
+    OUTSIDE = 0x20,
 }
 
 ShapeDrawer = {}
@@ -47,11 +48,22 @@ end
 
 function ShapeDrawer:drawText(x, y, boundingWidth, boundingHeight, text, alignment, colour)
     assert(text ~= nil, "Passed no text to draw")
-    local offsetX = 0
-    local offsetY = 0
+    local offsetX, offsetY = 0, 0
     local font = love.graphics.getFont()
     local textWidth = font:getWidth(text)
     local textHeight = font:getHeight(text)
+    if (bit.band(alignment,TextAlignment.OUTSIDE) == 0) then
+        offsetX, offsetY = self:insideAlignments(alignment, textWidth, textHeight, boundingWidth, boundingHeight)
+    else
+        offsetX, offsetY = self:outsideAlignments(alignment, textWidth, textHeight, boundingWidth, boundingHeight)
+    end
+    love.graphics.setColor(colour.r, colour.g, colour.b)
+    love.graphics.print(text, x + offsetX, y + offsetY)
+end
+
+function ShapeDrawer:insideAlignments(alignment, textWidth, textHeight, boundingWidth, boundingHeight)
+    local offsetX = 0
+    local offsetY = 0
     if (bit.band(alignment,TextAlignment.CENTRE) ~= 0) then
         offsetX = (boundingWidth - textWidth) / 2
         offsetY = (boundingHeight - textHeight) / 2
@@ -66,6 +78,25 @@ function ShapeDrawer:drawText(x, y, boundingWidth, boundingHeight, text, alignme
     elseif (bit.band(alignment,TextAlignment.BOTTOM) ~= 0) then
         offsetY = boundingHeight - textHeight
     end
-    love.graphics.setColor(colour.r, colour.g, colour.b)
-    love.graphics.print(text, x + offsetX, y + offsetY)
+    return offsetX, offsetY
+end
+
+function ShapeDrawer:outsideAlignments(alignment, textWidth, textHeight, boundingWidth, boundingHeight)
+    local offsetX = 0
+    local offsetY = 0
+    if (bit.band(alignment,TextAlignment.CENTRE) ~= 0) then
+        offsetX = (boundingWidth - textWidth) / 2
+        offsetY = (boundingHeight - textHeight) / 2
+    end
+    if (bit.band(alignment,TextAlignment.LEFT) ~= 0) then
+        offsetX = - textWidth
+    elseif (bit.band(alignment,TextAlignment.RIGHT) ~= 0) then
+        offsetX = boundingWidth + textWidth
+    end
+    if (bit.band(alignment,TextAlignment.TOP) ~= 0) then
+        offsetY = - textHeight
+    elseif (bit.band(alignment,TextAlignment.BOTTOM) ~= 0) then
+        offsetY = boundingHeight
+    end
+    return offsetX, offsetY
 end
