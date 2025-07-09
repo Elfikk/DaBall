f35c8aef57091f4d7ad286105a865c84a69a995c
Elfikk <Jaroslaw.Wiktor.Ciba@gmail.com>
2025-07-10 00:07:40 +0100

    Add Buttons!

---
+++ Button.lua
@@ -0,0 +1,37 @@
+require("Palettes")
+require("ShapeDrawer")
+
+Button = {
+    palette = CurrentTheme.counterColours,
+    id = 0,
+    text = nil,
+    textAlignment = 0,
+}
+
+function Button:new(x0, x1, y0, y1, text, textAlignment)
+    Button.__index = Button
+    setmetatable(Button, {__index = Box})
+    local o = Box:new(x0, x1, y0, y1)
+    o.id = Button.id
+    Button.id = Button.id + 1
+    o.text = text
+    o.textAlignment = textAlignment
+    return o
+end
+
+function Button:mousepressed(x, y, button, istouch, presses)
+    return self:inside(x, y)
+end
+
+function Button:draw(adapter)
+    local bounds = self.bounds
+    ShapeDrawer:drawRectangle(
+        bounds.x0,
+        bounds.y0,
+        bounds.x1 - bounds.x0,
+        bounds.y1 - bounds.y0,
+        self.palette,
+        self.text,
+        self.textAlignment
+    )
+end
---
+++ ButtonHandler.lua
@@ -0,0 +1,50 @@
+ButtonTypes = {
+    PAUSE = {},
+    RESTART = {},
+    QUIT = {},
+}
+
+ButtonHandler = {
+    visibleButtons = {},
+    allButtons = {},
+}
+
+function ButtonHandler:new()
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+function ButtonHandler:draw()
+    for enum, button in pairs(self.visibleButtons) do
+        button:draw()
+    end
+end
+
+function ButtonHandler:mousepressed(x, y, mouseButton, istouch, presses)
+    for enum, button in pairs(self.visibleButtons) do
+        if button:mousepressed(x, y, mouseButton, istouch, presses) then
+            return enum
+        end
+    end
+end
+
+function ButtonHandler:addButton(type, button, visible)
+    self.allButtons[type] = button
+    if visible then
+        self.visibleButtons[type] = button
+    end
+end
+
+function ButtonHandler:makeVisible(type)
+    assert(self.visibleButtons[type] == nil, "Button is already visible.")
+    assert(self.allButtons[type] ~= nil, "Button type does not exist but want to make visible.")
+    self.visibleButtons[type] = self.allButtons[type]
+end
+
+function ButtonHandler:hide(type)
+    assert(self.visibleButtons[type] ~= nil, "Button is already visible.")
+    assert(self.allButtons[type] ~= nil, "Button type does not exist but want to make visible.")
+    self.visibleButtons[type] = nil
+end
